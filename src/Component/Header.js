e0580443b4882bad94e4bccc12d97d6b36ff8d41
Code_Miles <devaze007@gmail.com>
2019-07-25 17:47:53 +0100

    mobile nav res responsiveness

---
+++ src/Component/Header.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import logo from './static/logo.png'
+import {Nav,Navbar,Container } from 'react-bootstrap';
+import '../styles/Nav.css';
+import search from './static/search.png';
+
+class Header extends Component {
+    render(){
+        return(
+            <div className='navs'>
+<Navbar collapseOnSelect expand="lg"  variant="light">
+    <Container>
+  <Navbar.Toggle id='toggle'/>
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Trending</Nav.Link>
+      <Nav.Link href="#pricing">All</Nav.Link>
+      <Nav.Link href="#pricing">Sources</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  <Navbar.Brand href="#home" id='logo'><img src={logo} alt='logo' /></Navbar.Brand>
+  <img src={search} alt='search' />
+  </Container>
+</Navbar>
+<Nav className="justify-content-center" activeKey="/home" variant="pills">
+    <Nav.Item>
+      <Nav.Link href="/home">Tech</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-1">Business</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2">Finance</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+    </Nav.Item>
+  </Nav>
+</div>
+        )
+    }
+}
+
+
+export default Header
