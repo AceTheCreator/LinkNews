24b6bf4bd543deb241d1b91be625f7b284301e2c
Code_Miles <devaze007@gmail.com>
2019-07-31 00:30:08 +0100

    Created Async Component to write a better code spliting for my routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import React, {Component} from 'react';
 import './App.css';
 import Trending from './Component/News';
+import AsyncComponent from './Component/AsyncComponent';
 
 class App extends Component {
   state={
     route:'trending',
-    component: ''
+    component: null
   }
 
   onRouteChange = (route) =>{
-    if(route === 'trending'){
       this.setState({route:route})
-    }else if (route === 'all'){
-      import('./Component/All').then((All)=>{
-        console.log(All)
-        this.setState({route:route, component: All.default})
-      })
-    }
   }
 
   render(){
     
     if(this.state.route === 'trending'){
      return<Trending onRouteChange = {this.onRouteChange} />
-    }else {
-      return <this.state.component onRouteChange={this.onRouteChange} />
+    }else if (this.state.route === 'all'){
+      const AsyncAllNews = AsyncComponent(()=> import('./Component/All'))
+      return <AsyncAllNews onRouteChange = {this.onRouteChange} />
     }
   }
 
