603037c2c470d06c3634428f49a43712cf587c33
Code_Miles <devaze007@gmail.com>
2019-07-31 18:02:31 +0100

    Updates

---
+++ precache-manifest.1af095a4ac3f9551c8a007726f4388bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7699385c8a24b7f70defefba86dca730",
+    "url": "/LinkNews/index.html"
+  },
+  {
+    "revision": "4c6079c6f2ea7dd718b3",
+    "url": "/LinkNews/static/css/main.ccc7b377.chunk.css"
+  },
+  {
+    "revision": "21d85247c88390e1a045",
+    "url": "/LinkNews/static/js/2.120f35f0.chunk.js"
+  },
+  {
+    "revision": "9861c70c96b79bad42ce",
+    "url": "/LinkNews/static/js/3.60d4e610.chunk.js"
+  },
+  {
+    "revision": "4c6079c6f2ea7dd718b3",
+    "url": "/LinkNews/static/js/main.48934bf5.chunk.js"
+  },
+  {
+    "revision": "b37d818048426bfd79f5",
+    "url": "/LinkNews/static/js/runtime~main.50e0d37b.js"
+  },
+  {
+    "revision": "abe7719808ff68530bea24e987e474f1",
+    "url": "/LinkNews/static/media/logo.abe77198.png"
+  }
+]);
